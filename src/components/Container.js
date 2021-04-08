78f80af3293405163e027167b006b4ede69cde19
wojciechlasak <wojciech.lasak@student.uj.edu.pl>
2021-04-08 10:43:22 +0200

    added air

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,14 @@
 import React, { useRef } from 'react';
+import Air from './Air';
 
 const Container = ({ children, name, title }) => {
   const ref = useRef(null);
   return (
     <div className="content column flexc separator">
+      <Air />
       <h2>{title}</h2>
       {children}
+      <Air />
     </div>
   );
 };
